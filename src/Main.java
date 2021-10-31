f8e34df1619bb65462571d44397f9ea1e45119c3
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-31 03:36:31 -0300

    for- contagem regressiva

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		for(int i=0; i <5; i++) {
-			System.out.println("Valor de i: "+i);
+		for(int i= 4; i > 0; i--) {
+			System.out.println("Valor de i: "+ i);
 		}
 		
 	}
