e8944ce3b202c3f55651b6f95015b5051052c621
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-31 03:20:27 -0300

    for- concatenado com i

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,13 @@
-import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 	
-		/* for=para (Fazer um programa que lê o valor inteiro N vezes.
-		 depois N números inteiros. Ao final, mostra a soma dos N números lidos.)*/
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		
-		int soma = 0;
-		for (int i = 0; i < N; i++) {
-			int x = sc.nextInt();
-			soma = soma + x;
-			
+		for(int i=0; i <5; i++) {
+			System.out.println("Valor de i: "+i);
 		}
 		
-		System.out.println(soma);
-		sc.close();
-		
-			
-
 	}
 
 }
