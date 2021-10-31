5c78b9d0505386aae4682a0f294bcbbd4d86359d
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-31 04:28:50 -0300

    for-teste de mesa

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		for(int i= 4; i > 0; i--) {
-			System.out.println("Valor de i: "+ i);
+		int x= 3;
+		int y= 0;
+		
+		for( int i= 0; i < x ; i++) {
+			System.out.print(i + ",");
+			y = y + 5;
+			System.out.println(y);
 		}
 		
 	}
