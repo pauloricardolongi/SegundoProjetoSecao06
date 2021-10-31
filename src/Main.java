e80bca834ea70546a4f013e27e8a66e2df088fc5
pauloricardolongi <pauloricardolongi@gmail.com>
2021-10-31 03:00:58 -0300

    continuação for

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		/* for=para (Fazer um programa que lê o valor inteiro N vezes.
+		 depois N números inteiros. Ao final, mostra a soma dos N números lidos.)*/
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int soma = 0;
+		for (int i = 0; i < N; i++) {
+			int x = sc.nextInt();
+			soma = soma + x;
+			
+		}
+		
+		System.out.println(soma);
+		sc.close();
+		
+			
 
 	}
 
