47e6178aa5f0e3d3632243b135bcb3877100ede7
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-01 14:12:13 -0300

    primeiro programa while

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,32 @@
-
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-	
-		int x= 3;
-		int y= 0;
+	/* Fazer um programa para ler uma temperatura em Celsius e mostrar o equivalente me Fahrenhei. Perguntar se o usuário deseja
+	 repetir(s/n). Caso o usuário digite "s", repetir o programa.
+	 fórmula: F= 9C/5 +32. Exemplo: 1- digite a temperatura em Celsius:30; 2- equivalente em Fahrenheit:86.0
+	 Deseja repeir (s/n)? */
+		Locale.setDefault(Locale.US);
+		Scanner sc= new Scanner(System.in);
 		
-		for( int i= 0; i < x ; i++) {
-			System.out.print(i + ",");
-			y = y + 5;
-			System.out.println(y);
+		System.out.print("Digite a temperatura em Celsius: ");
+		double C = sc.nextDouble();
+		double F = 9.0 * C / 5.0 + 32.0;
+		System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+		System.out.print("Deseja repetir (s/n)?");
+		char resposta = sc.next().charAt(0);//li o caracter s/n
+		while (resposta != 'n'){
+			System.out.print("Digite a temperatura em Celsius: ");
+		    C = sc.nextDouble();
+		    F = 9.0 * C / 5.0 + 32.0;
+			System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+			System.out.print("Deseja repetir (s/n)?");
+		    resposta = sc.next().charAt(0);  //montar uma estrutura que fica repetindo enquanto a resposta não for n
 		}
 		
+		sc.close();
 	}
 
 }
